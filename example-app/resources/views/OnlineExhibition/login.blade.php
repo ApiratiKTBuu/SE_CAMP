b8a5be862aa1320dc65c23a58416cfcf450baa7a
ApiratiKTBuu <65160365@go.buu.ac.th>
2024-02-22 00:49:02 +0700

    add google login

--- example-app/resources/views/OnlineExhibition/login.blade.php
+++ example-app/resources/views/OnlineExhibition/login.blade.php
@@ -5,6 +5,8 @@
         <title>Online Exhibition | Login</title>
         <link rel="stylesheet" href="{{ url('assets/dist/css/OnlineExhibition/login.css') }}">
         <script src="https://kit.fontawesome.com/a87b92189d.js" crossorigin="anonymous"></script>
+        <script src="https://apis.google.com/js/platform.js" async defer></script>
+        <meta name="google-signin-client_id" content="201955031781-vmjepq48hontk7v6svrpu549ipd4u5p5.apps.googleusercontent.com">
     </head>
     <body>
         <img src="assets/dist/img/myimg/Img_OnlineExhibition_logo_large_1.png" class="logo">
@@ -21,8 +23,19 @@
             <br>
             <a href="#" class="password-reset-link">Forgot your password?</a>
         <button type="submit" class="login-button">Login</button>
-        <button type="button" class="google-login-button"><i class="fa-brands fa-google"></i>      Login with Google</button>
+        <div class="g-signin2" data-onsuccess="onSignIn">
+        <button type="button" id="google-login-button"><i class="fa-brands fa-google"></i>      Login with Google</button>
+    </div>
         </form>
         </div>
     </body>
+    <script>
+        function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+    </script>
 </html>
